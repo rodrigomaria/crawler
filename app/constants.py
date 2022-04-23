6f205bffb22db9f7a950b1b585fb5da212c18f63
Rodrigo Maria Morgão <rodrigo@rodrigomaria.com.br>
2022-04-23 02:06:38 -0300

    feat: configured CONSTANTS

---
+++ app/constants.py
@@ -0,0 +1,3 @@
+PAGE_TARGET = {1: "https://www.vultr.com/products/bare-metal/#pricing", 2: "https://www.hostgator.com/vps-hosting"}
+
+FILES = {"csv": "./data_files/file.csv", "json": "./data_files/file.json"}
